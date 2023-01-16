46c0362eab1ff3b36b595fd3a6a31eae8d8b3110
Jags <jon.kmjt@gmail.com>
2023-01-16 16:50:21 -0500

    majority of functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ searchBtn.addEventListener('click', citySearch);
 
 function citySearch() {
     var searchTxt = document.querySelector('#searchText').value;
+    handleRecentSearch(searchTxt);
 
     var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?appid=f04a58c9a2e1f1ef45516c6f6b1c8eb6&units=imperial&q=" + searchTxt;
 
@@ -37,43 +38,69 @@ function citySearch() {
             var currentHum = document.querySelector('#currentCityHum');
             currentHum.textContent = "Humidity: " + data.list[0].main.humidity + " %";
 
+            //for loop to get the next 5 day forecast
+            var fiveDaySec = document.querySelector('#fiveDay');
+            fiveDaySec.replaceChildren(); //clears the divs that were created dynamically
+
+            for(var i = 1; i < data.list.length; i++){
+                if(data.list[i].dt_txt.includes('15:00:00')){
+                    var fiveDayDiv = document.createElement('div');
+                    var fdDate = document.createElement('h3');
+
+                    //creates the divs dynamically 
+                    fiveDaySec.appendChild(fiveDayDiv);
+                    fiveDayDiv.style.backgroundColor = 'gray';
+                    fiveDayDiv.appendChild(fdDate);
+                    fiveDayDiv.style.display = 'inline-block';
+                    fiveDayDiv.style.maxWidth = '15%';
+                    fiveDayDiv.style.marginRight = '25px';
+
+                    //for each day get the corresponding date
+                    day = data.list[i].dt_txt.split(' ');
+                    fdDate.textContent = day[0]; //gets the first half of the date
+                    fdDate.style.color = 'white';
+
+                    //creates img element and finds the corresponding image via the code from data using the url as a src
+                    iconCode = data.list[i].weather[0].icon;
+                    var wImage = document.createElement('img');
+                    wImage.src = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+
+                    fdDate.appendChild(wImage); //appends image to date
+
+                    //create and append temp values
+                    var tempH4 = document.createElement('h4');
+                    fiveDayDiv.appendChild(tempH4);
+                    tempH4.textContent = "Temp: " + data.list[i].main.temp + "\u00B0F";
+                    tempH4.style.color = "white";
 
 
-        
+                    //create and append wind values
+                    var windH4 = document.createElement('h4');
+                    fiveDayDiv.appendChild(windH4);
+                    windH4.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
+                    windH4.style.color = "white";
+
+                    //create and append humidity values
+                    var humH4 = document.createElement('h4');
+                    fiveDayDiv.appendChild(humH4);
+                    humH4.textContent = "Humidity: " + data.list[i].main.humidity + " %";
+                    humH4.style.color = "white";
+
+                }
+            }    
     })
-
-
 }
 
-/*function citySearch() {
-    var searchTxt = document.querySelector('#searchText').value;
-    //console.log(searchTxt);
-    var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?&appid=f04a58c9a2e1f1ef45516c6f6b1c8eb6&q=" + searchTxt;
-    
-    fetch(geoUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data[0]);
-            var cityLat = data[0].lat;
-            var cityLon = data[0].lon;
+function handleRecentSearch(search) {
+    var recent = document.querySelector('#recentSearch');
+    var recentBtn = document.createElement('button');
 
-            getCityWeather(cityLat,cityLon);
-    })
+    recent.appendChild(recentBtn);
+    recentBtn.textContent = search;
+    recentBtn.style.width = '100%';
+    recentBtn.style.margin = '5px';
+
+    recentBtn.addEventListener('click', function(){
+        document.querySelector('#searchText').value = search;
+    });
 }
-
-function getCityWeather(lat, lon) {
-    var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon +"&appid=f04a58c9a2e1f1ef45516c6f6b1c8eb6"
-
-    fetch(weatherUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-    })
-
-
-
-}*/
